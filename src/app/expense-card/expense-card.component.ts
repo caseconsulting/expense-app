0df8dc23b7e3d04d93d16cc3fe50b88fe78ebc4d
Fahad <fahada95@vt.edu>
2017-08-15 09:36:24 -0400

    dashboard: dashboard checkflag undo implemented

--- src/app/expense-card/expense-card.component.ts
+++ src/app/expense-card/expense-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Employee, EmployeeService} from '../employee/employee.service';
-import { Expense, ExpenseService} from '../expense/expense.service';
-import { ExpenseType, ExpenseTypeService} from '../expense-type/expense-type.service';
+import { Employee, EmployeeService } from '../employee/employee.service';
+import { Expense, ExpenseService } from '../expense/expense.service';
+import { ExpenseType, ExpenseTypeService } from '../expense-type/expense-type.service';
 import { UpdateListService } from '../update-list.service';
 import { Subscription } from 'rxjs/Subscription';
 import { Router } from '@angular/router';
@@ -14,29 +14,57 @@ import { ErrorService } from '../error/error.service';
 export class ExpenseCardComponent implements OnInit {
   @Input() model: Expense;
   reimburseFlag: boolean;
+  expenseType: ExpenseType;
+  employee: Employee;
   constructor(
-  private employeeService: EmployeeService,
-  private expenseService: ExpenseService,
-  private updateListService: UpdateListService,
-  private router: Router,
-  private errorService: ErrorService) { }
+    private employeeService: EmployeeService,
+    private expenseService: ExpenseService,
+    private expenseTypeService: ExpenseTypeService,
+    private updateListService: UpdateListService,
+    private errorService: ErrorService,
+    private router: Router) { }
 
-ngOnInit() {
-  if (this.model.reimbursedDate === 'not assigned') {
-    this.reimburseFlag = false;
-  } else {
-    this.reimburseFlag = true;
-}
-}
+  ngOnInit() {
+    if (this.model.reimbursedDate === 'not assigned') {
+      this.reimburseFlag = false;
+    } else {
+      this.reimburseFlag = true;
+    }
+    this.getExpenseType();
+    this.getEmployee();
+  }
   updateDate(val: any) {
-    const today = new Date();
-    console.log('val: ', val, 'reimburseFlag', this.reimburseFlag);
-    console.log('today', today )
-    this.model.reimbursedDate = today;
-    this.expenseService.updateExpense(this.model)
-    .subscribe(
-      res => {},
+    let update;
+    if (this.reimburseFlag === true) {
+      const today = new Date();
+      console.log('val: ', val, 'reimburseFlag', this.reimburseFlag);
+      console.log('today', today)
+      this.model.reimbursedDate = today;
+      update = this.expenseService.updateExpense(this.model);
+
+    } else {
+      this.model.reimbursedDate = 'not assigned';
+      update = this.expenseService.updateExpense(this.model);
+
+    }
+    update.subscribe(
+      res => { },
       error => this.errorService.announceError({ status: error, type: 'Employees' })
-    )
+    );
+  }
+
+  getExpenseType() {
+    this.expenseTypeService.readSingleExpenseType(this.model.expenseTypeId)
+      .subscribe(
+      returnedExpenseType => this.expenseType = returnedExpenseType,
+      error => this.errorService.announceError({ status: error, type: 'Expense Type' })
+      );
+  }
+  getEmployee() {
+    this.employeeService.readSingleEmployee(this.model.userId)
+      .subscribe(
+      returnedEmployee => this.employee = returnedEmployee,
+      error => this.errorService.announceError({ status: error, type: 'Employee' })
+      );
   }
 }
